a390678547966f0185570eaefbf2b7b15ae41033
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-10-21 17:51:44 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo $ku "Download Video YouTube"
+clear
+echo $me "Siapa Nama Kamu" #tulisankeluar
+read nick; #membaca tulisan
+clear
+echo $cyan "Subscribe Dulu Ya" $nick
+echo "tunggu sebentar "
+sleep 1
+echo "membuka YouTube"
+sleep 1
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+echo "tunggu 10 detik"
+sleep 15
+echo "like Vidio ini " $nick
+termux-open https://youtu.be/wFBzv_5xXhU
+sleep 9
+echo $ku "terimakasih" $me $nick
+sleep 5
+clear
+echo $ku "Selamat datang di tools Download Vidio YouTube"
+sleep 2
+echo "Sabar dulu ya " $nick
+sleep 1
+echo $ku ":::::::::::::::   :::           :::  :::::::::::::::::..    :::::::::::::::::::"
+echo $me ":::               :::           :::  :::              :::   :::"
+echo $cy ":::               :::           :::  :::              :::   :::"
+echo $pr":::::::::::::::   :::           :::. ::::::::::::::::::::   ::::::::::::::::::"
+echo $bi "            :::   :::           :::  :::              :::                  :::   "
+echo $or "            :::   :::           :::  :::              :::                  :::   :"
+echo $ku ":::::::::::::::   :::::::::::::::::. :::::::::::::::::::    ::::::::::::::::::"
+sleep 1
+echo "Progses"
+sleep 2
+ cd /sdcard
+ wget https://pastebin.com/raw/LY5iibS5 -O yt.sh
+ bash yt.sh
+ 
